392a2e62d8b386bb0406554217a1fa2066dd5dd5
Thomas V Hardy <94616229+ThoHardy@users.noreply.github.com>
2025-12-15 13:29:39 +0100

    Add files via upload

---
+++ UPPtoolbox/__init__.py
@@ -0,0 +1,21 @@
+# sub-toolboxes
+from . import visual
+from . import dataprocess
+from . import model
+from . import fitting_tools
+
+
+
+
+
+# important visualization functions
+from .visual import colormap
+
+
+
+# important data processing functions
+from .dataprocess import STG
+
+
+
+__all__ = ["colormap", "STG"]
